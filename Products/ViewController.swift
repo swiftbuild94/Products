638273d7d46bd3bcdcd1837c1e2c8dc79713cf0e
Patricio Benavente <benavente.patricio@facebook.com>
2019-03-24 21:09:36 -0300

    Initial Commit

---
+++ Products/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Products
+//
+//  Created by Patricio Benavente on 24/03/19.
+//  Copyright © 2019 Patricio Benavente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
